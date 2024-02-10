6f1fc04aa0c770e3740f664d9b864c2f10cc97ae
bisirinivas <bisirinivas>
2024-02-10 20:05:53 +0000

    adda

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "rgname" {
+  type        = string
+  description = "Resource group name"
+
+}
+
+variable "location" {
+
+  type = string
+
+  description = "Resource Group location"
+
+
+}
