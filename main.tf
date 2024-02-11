ca6f0933e99b026fccd035e4471df65b1b373759
bisirinivas <bisirinivas>
2024-02-11 10:37:10 +0000

    da

--- main.tf
+++ main.tf
@@ -26,7 +26,6 @@ resource "azurerm_subnet" "subnet1" {
     name = var.subnet
     virtual_network_name = azurerm_virtual_network.vm1
     resource_group_name = azurerm_resource_group.rg.name
-    location = azurerm_resource_group.location
     address_prefixes =["10.0.2.0/24"]
   
 }
