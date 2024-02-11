325b65477dd710c94ade43e4eb79dc584ef857d1
bisirinivas <bisirinivas>
2024-02-11 10:02:22 +0000

    update var file

--- variables.tf
+++ variables.tf
@@ -12,3 +12,51 @@ variable "location" {
 
 
 }
+
+
+variable "vnname" {
+
+  type = string
+
+  description = "vm name"
+
+}
+
+variable "subnet1" {
+
+  type        = string
+  description = "subnet name"
+
+}
+
+variable "nsg1" {
+
+  type = string
+
+  description = "nsg name"
+
+
+}
+
+
+variable "nic1" {
+
+  type = string
+
+  description = "nic name"
+}
+
+variable "" {
+
+}
+
+variable "vmname" {
+
+  type = string
+
+
+  description = "vm name"
+
+}
+
+
