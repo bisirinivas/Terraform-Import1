4c8ebda2c4f243f7836d2aa877d59327bdfec8aa
bisirinivas <bisirinivas>
2024-02-11 09:44:49 +0000

    added vnet subnet nsg and associate group

--- main.tf
+++ main.tf
@@ -1,13 +1,22 @@
 resource "azurerm_resource_group" "rg" {
 
-name = var.rgname
-location = var.location
+  name     = var.rgname
+  location = var.location
 
 }
 
 resource "azurerm_resource_group" "rg1" {
 
-name = "rgs"
-location = "UK South"
+  name     = "rgs"
+  location = "UK South"
+
+}
+
+
+resource "azurerm_virtual_network" "vm" {
+  name                = var.vmname
+  resource_group_name = var.rgname
+  location            = var.location
+  address_space       = ["10.0.0.0/16"]
 
 }
