3a550c1f9c6a47fac98905e07f4c386fb9b9fad0
bisirinivas <bisirinivas>
2024-02-11 10:49:48 +0000

    modfied all resources

--- main.tf
+++ main.tf
@@ -21,79 +21,69 @@ resource "azurerm_virtual_network" "vm1" {
   
 }
 
-
 resource "azurerm_subnet" "subnet1" {
-    name = var.subnet
-    virtual_network_name = azurerm_virtual_network.vm1
-    resource_group_name = azurerm_resource_group.rg.name
-    address_prefixes =["10.0.2.0/24"]
-  
+    name                 = var.subnet_name
+    virtual_network_name = azurerm_virtual_network.vm1.name
+    resource_group_name  = azurerm_resource_group.rg.name
+    address_prefixes     = ["10.0.2.0/24"]
 }
 resource "azurerm_network_security_group" "nsg1" {
-    name = var.nsg
-    resource_group_name = azurerm_resource_group.rg.name
-    location = azurerm_resource_group.rg.location
+  name                = var.nsg_name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
 
-    security_rule {
-
-
-    name = "nsgrules"
-    priority = 101
-    direction = "Inbound"
-    access = "allow"
-    protocol = "Tcp"
-    source_port_range = "*"
-    destination_port_range = "*"
-    source_address_prefix = "*"
+  security_rule {
+    name                       = "test123"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
     destination_address_prefix = "*"
-    }
+  }
 }
-
 resource "azurerm_subnet_network_security_group_association" "subnetassociate" {
-  
-    subnet_id = azurerm_subnet.subnet1
-    network_security_group_id = azurerm_network_security_group.nsg1
+    subnet_id                 = azurerm_subnet.subnet1.id
+    network_security_group_id = azurerm_network_security_group.nsg1.id
 }
 
 resource "azurerm_network_interface" "nic1" {
-  name                = var.nic
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+    name                = var.nic_name
+    location            = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
 
-  ip_configuration {
-    name                          = "nic-configuration"
-    subnet_id                     = azurerm_subnet.subnet1.id
-    private_ip_address_allocation = "Dynamic"
-  }
+    ip_configuration {
+        name                          = "nic-configuration"
+        subnet_id                     = azurerm_subnet.subnet1.id
+        private_ip_address_allocation = "Dynamic"
+    }
 }
 
 resource "azurerm_network_interface_security_group_association" "nicinterface" {
-  network_interface_id      = azurerm_network_interface.nic1
-  network_security_group_id = azurerm_network_security_group.nsg1.id
+    network_interface_id      = azurerm_network_interface.nic1.id
+    network_security_group_id = azurerm_network_security_group.nsg1.id
 }
-
 
 resource "azurerm_linux_virtual_machine" "vm1" {
-  name                  = var.vmname
-  resource_group_name   = azurerm_resource_group.rg.name
-  location              = azurerm_resource_group.rg.location
-  size                  = "Standard_DS1_v2"
-  admin_username        = "adminuser"
-  admin_password        = "Password1234!"
-  network_interface_ids = [azurerm_network_interface.nic1.id]
+    name                  = var.vmname
+    resource_group_name   = azurerm_resource_group.rg.name
+    location              = azurerm_resource_group.rg.location
+    size                  = "Standard_DS1_v2"
+    admin_username        = "adminuser"
+    admin_password        = "Password1234!"
+    network_interface_ids = [azurerm_network_interface.nic1.id]
 
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
+    os_disk {
+        caching              = "ReadWrite"
+        storage_account_type = "Standard_LRS"
+    }
 
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
-    version   = "latest"
-  }
+    source_image_reference {
+        publisher = "Canonical"
+        offer     = "UbuntuServer"
+        sku       = "16.04-LTS"
+        version   = "latest"
+    }
 }
-    
-
-
