c834de22fd33789d911959ead97c7f4c5ed0fc4f
bisirinivas <bisirinivas>
2024-02-11 11:16:58 +0000

    dah

--- main.tf
+++ main.tf
@@ -67,23 +67,25 @@ resource "azurerm_network_interface_security_group_association" "nicinterface" {
 }
 
 resource "azurerm_linux_virtual_machine" "vm1" {
-    name                  = var.vmname
-    resource_group_name   = azurerm_resource_group.rg.name
-    location              = azurerm_resource_group.rg.location
-    size                  = "Standard_DS1_v2"
-    admin_username        = "adminuser"
-    admin_password        = "Password1234!"
-    network_interface_ids = [azurerm_network_interface.nic1.id]
+  name                  = var.vmname
+  resource_group_name   = azurerm_resource_group.rg.name
+  location              = azurerm_resource_group.rg.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  admin_password      = "P@$$w0rd1234!"
+  network_interface_ids = [
+    azurerm_network_interface.nic1.id,
+  ]
 
-    os_disk {
-        caching              = "ReadWrite"
-        storage_account_type = "Standard_LRS"
-    }
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
 
-    source_image_reference {
-        publisher = "Canonical"
-        offer     = "UbuntuServer"
-        sku       = "16.04-LTS"
-        version   = "latest"
-    }
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
 }
