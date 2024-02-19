68c3de1e88d8cc7f98956c1f0b8d7608fd07eeb0
bisirinivas <127945511+bisirinivas@users.noreply.github.com>
2024-02-19 13:28:31 +0000

    Update login unbuntu Vm ssh.tf

--- login unbuntu Vm ssh.tf
+++ login unbuntu Vm ssh.tf
@@ -1,5 +1,10 @@
 ssh -i agentpool_key.pem  azureuser@20.117.190.82
 
+chmod 600 agentpool_key.pem
+
+then run again  ssh -i agentpool_key.pem  azureuser@20.117.190.82
+
+
 run agent in the ubuntu vm 
 
 ./run.sh
