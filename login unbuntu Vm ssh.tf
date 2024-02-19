1182184f818dfe0975916017cb7022ec78165483
bisirinivas <127945511+bisirinivas@users.noreply.github.com>
2024-02-19 14:03:12 +0000

    Update login unbuntu Vm ssh.tf

--- login unbuntu Vm ssh.tf
+++ login unbuntu Vm ssh.tf
@@ -4,6 +4,12 @@ chmod 600 agentpool_key.pem
 
 then run again  ssh -i agentpool_key.pem  azureuser@20.117.190.82
 
+1.created agent and selct selfhosted agent
+2.mkdir myagent && cd myagen 
+3.https://vstsagentpackage.azureedge.net/agent/3.234.0/vsts-agent-linux-x64-3.234.0.tar.gz
+4.tar zxvf ~/Downloads/vsts-agent-linux-x64-3.234.0.tar.gz
+5.sudo apt update
+6.
 
 run agent in the ubuntu vm 
 
