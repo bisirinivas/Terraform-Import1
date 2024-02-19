9720e2ded8dd6ab575f3fdbe9a2ea3eef7d1979f
bisirinivas <127945511+bisirinivas@users.noreply.github.com>
2024-02-19 14:51:27 +0000

    Update login unbuntu Vm ssh.tf

--- login unbuntu Vm ssh.tf
+++ login unbuntu Vm ssh.tf
@@ -9,7 +9,13 @@ then run again  ssh -i agentpool_key.pem  azureuser@20.117.190.82
 3.https://vstsagentpackage.azureedge.net/agent/3.234.0/vsts-agent-linux-x64-3.234.0.tar.gz
 4.tar zxvf ~/Downloads/vsts-agent-linux-x64-3.234.0.tar.gz
 5.sudo apt update
-6.
+6../config.sh
+7.Enter server URL > https://dev.azure.com/yourorgname
+8.PAT tocken (azurecicd web -usersetting-will see pat token 
+9.add agent name
+10../run.sh
+
+
 
 run agent in the ubuntu vm 
 
