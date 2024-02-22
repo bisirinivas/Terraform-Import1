d8755321e63a531a7a04b9256970fe0e39fd20c6
bisirinivas <127945511+bisirinivas@users.noreply.github.com>
2024-02-22 17:00:43 +0000

    Create acrcreatsecret.tf
    acr create

---
+++ acrcreatsecret.tf
@@ -0,0 +1,5 @@
+kubectl create secret docker-registry acr-secret \
+--docker-server=https://azurecicdcontainer.azurecr.io \
+--docker-username=azurecicdcontainer \
+--docker-password=lOB+ddOhwmX1pT6JltTA0DRauBrhIvCLjMToSNjfRv+ACRB/2zha \
+--docker-email=bisirinivas@gmail.com
